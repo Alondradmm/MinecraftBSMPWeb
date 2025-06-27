d4d7b70cbba163a76b89ddcef659f1d48d295aa9
Alondradmm <dalon0904@gmail.com>
2025-06-27 14:33:20 -0600

    Se eliminó el modal

--- js/modal.js
+++
@@ -1,8 +0,0 @@
-// Función --> Asignar al Botón
-function OpenModal(){
-    document.getElementById("modal").style.display='flex'
-}
-
-function CloseModal(){
-    document.getElementById("modal").style.display='none'
-}
